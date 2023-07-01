ae5005b821ae60cb45d93017f95614accb14700e
yifan <yifanchen0914@gmail.com>
2023-07-01 11:49:24 +0800

    [fix]: debug

--- src/main/java/com/springbootbasic/config/RestTemplateConfig.java
+++ src/main/java/com/springbootbasic/config/RestTemplateConfig.java
@@ -8,24 +8,25 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate getRestTemplate(ClientHttpRequestFactory clientHttpRequestFactory) {
-        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+    public RestTemplate getRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters()
-                .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
         return restTemplate;
     }
 
-    @Bean
-    public ClientHttpRequestFactory getClientHttpRequestFactory() {
-        int timeout = 5000;
-        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
-                = new HttpComponentsClientHttpRequestFactory();
-        clientHttpRequestFactory.setConnectTimeout(timeout);
-        return clientHttpRequestFactory;
-    }
+//    @Bean
+//    public ClientHttpRequestFactory getClientHttpRequestFactory() {
+//        int timeout = 5000;
+//        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+//                = new HttpComponentsClientHttpRequestFactory();
+//        clientHttpRequestFactory.setConnectTimeout(timeout);
+//        return clientHttpRequestFactory;
+//    }
 }
